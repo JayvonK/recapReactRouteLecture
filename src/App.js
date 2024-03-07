f91b4eb8b727fa094c1781e8d0685dd100caf765
JayvonK <jaykhuth@gmail.com>
2024-03-07 10:05:21 -0800

    creating jacoozzi page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePageComponent from './Components/Pages/HomePageComponent';
+import JacoozziPageComponent from './Components/Pages/JacoozziPageComponent';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       {/* Route defines our routes to different pages */}
       {/* '/' defines our homepage or the default rendering page */}
       <Route path='/' element={<HomePageComponent />}/>
+      <Route path='/jacoozzi' element={<JacoozziPageComponent/>} />
     </Routes>
 
     </BrowserRouter>
---
+++ src/Components/Pages/JacoozziPageComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JacoozziPageComponent = () => {
+  return (
+    <div className='d-flex justify-content-end'>
+      <h1>This is Jacoozzi's Page</h1>
+    </div>
+  )
+}
+
+export default JacoozziPageComponent
