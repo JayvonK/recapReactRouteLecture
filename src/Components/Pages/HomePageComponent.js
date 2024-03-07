162ad33f427d600e2dd0a4616fe87f227cacb8fb
JayvonK <jaykhuth@gmail.com>
2024-03-07 09:58:49 -0800

    first commit

---
+++ src/Components/Pages/HomePageComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePageComponent = () => {
+  return (
+    <div className='d-flex justify-content-center'>
+      <h1>This is our Home Page</h1>
+    </div>
+  )
+}
+
+export default HomePageComponent
